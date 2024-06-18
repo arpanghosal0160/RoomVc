32b1d9618618b713f81406df6d8a44a1a671eb2f
Arpan Ghosal <arpanghosal0160@gmail.com>
2024-06-18 19:10:59 +0530

    removed upcoming meeting bug

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -9,7 +9,7 @@ const Home = () => {
     <section className="flex size-full flex-col gap-10 text-white">
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lag:p-11">
-          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">Upcoming Meeting at: 12:30 PM</h2>
+          
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">
               {time}
